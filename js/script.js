afbfc6a08e0e358ca5d83ab7ad36b3db5d22fbd8
Lucas Prates <lucas.prates@matrixdobrasil.com.br>
2023-11-03 03:23:26 -0300

    feat: add gameOverImage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
+const gameOverImage = document.querySelector('.gameover');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -28,19 +29,23 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = "./imagens/game-over.png";
+        mario.src = "./imagens/game-over-mario.png";
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
 
         clouds.style.animation = 'none';
         clouds.style.right = cloudsPosition + 'px';
 
+        gameOverImage.src = "./imagens/game-over-image.png";
+        gameOverImage.style.width = '400px';
+        gameOverImage.style.position = 'absolute';
+        gameOverImage.style.left = '50%';
+        gameOverImage.style.transform = 'translateX(-50%)'; 
+
         clearInterval(loop)
     }
 
 }, 10);
-
-
 
 document.addEventListener('keydown', (event) => {
     if (event.key === ' ' || event.key === 'ArrowUp') {
